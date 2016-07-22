81ac0e5606577d2e2003e3e83e0f8a528949a735
769930719 <quzongbao521@qq.com>
2016-07-22 22:46:29 +0800

    初始页

--- note/routes/index.js
+++ note/routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'MyProject' });
 });
+/*系统管理*/
+router.get('/system', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
+
